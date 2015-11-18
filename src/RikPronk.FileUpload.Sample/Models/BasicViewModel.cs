014f3a4e409ba16a80e89504a14c26797438e319
Rik Pronk <rikpronkie@gmail.com>
2015-11-18 10:36:44 +0100

    I keep missing files...

---
+++ src/RikPronk.FileUpload.Sample/Models/BasicViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace RikPronk.FileUpload.Sample
+{
+    public class BasicViewModel
+    {
+        [Required]
+        public HttpPostedFileBase[] File { get; set; }
+    }
+}
