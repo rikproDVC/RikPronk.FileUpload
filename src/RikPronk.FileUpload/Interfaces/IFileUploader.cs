eeb50bfc053731195642b33b7a8efd210c33fbb6
Rik Pronk <rikpronkie@gmail.com>
2015-11-18 10:00:39 +0100

    Moved interfaces to seperate folder

--- src/RikPronk.FileUpload/Interfaces/IFileUploader.cs
+++ src/RikPronk.FileUpload/Interfaces/IFileUploader.cs
@@ -1,4 +1,4 @@
-﻿using System.IO;
+﻿using System.Collections;
 using System.Threading.Tasks;
 
 namespace RikPronk.FileUpload.Core
@@ -6,6 +6,10 @@ namespace RikPronk.FileUpload.Core
     public interface IFileUploader
     {
         void Upload();
+    }
+
+    public interface IAsyncFileUploader : IFileUploader
+    {
         Task UploadAsync();
     }
 }
