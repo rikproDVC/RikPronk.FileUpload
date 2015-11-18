d19f2feae7da598cea2d40ff7c3ccaaad9470e4f
Rik Pronk <rikpronkie@gmail.com>
2015-11-18 10:02:54 +0100

    Created basic uploading sample

--- src/RikPronk.FileUpload.Sample/Controllers/HomeController.cs
+++ src/RikPronk.FileUpload.Sample/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using RikPronk.FileUpload.Core;
+using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace RikPronk.FileUpload.Sample.Controllers
 {
@@ -6,6 +8,27 @@ namespace RikPronk.FileUpload.Sample.Controllers
     {
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult BasicUpload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> BasicUpload(BasicViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var fileCollection = UploadableFileCollection.From<UploadableFile>(viewModel.File);
+
+            var uploader = new AzureBlobStorageUploader(fileCollection, MvcApplication.GetAzureStorageContainer("basic-container"));
+            await uploader.UploadAsync();
+
             return View();
         }
     }
